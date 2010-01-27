276d25171dfc14285b9b264332c021009fe3aa25
Kevin Risden <compuwizard123@gmail.com>
2010-01-27 15:16:56 -0500

    Working buttons :-)

--- src/RedBlackTreeApplet.java
+++ src/RedBlackTreeApplet.java
@@ -1,16 +1,30 @@
-import java.applet.Applet;
 import java.awt.*;
+import java.applet.*;
 
+public class RedBlackTreeApplet extends Applet {
+	Button increase, decrease;
+	int value = 1000;
 
-public class RedBlackTreeApplet extends Applet{
-	public void init() { }
-	 
-	  // This method is mandatory, but can be empty.
-	  public void stop() { }
-	 
-	  // Print a message on the screen (x=20, y=10).
-	  public void paint(Graphics g) {
-	    g.drawString("Hello, world!", 20,10);
-	  }
+	public void init() {
+		increase = new Button("Increase the value");
+		add(increase);
+		decrease = new Button("Decrease the value");
+		add(decrease);
+	}
 
+	public void stop() {
+	}
+
+	public boolean action(Event e, Object args) {
+		if (e.target == increase)
+			value++;
+		if (e.target == decrease)
+			value--;
+		repaint();
+		return true;
+	}
+
+	public void paint(Graphics g) {
+		g.drawString("The value is currently " + value, 50, 80);
+	}
 }

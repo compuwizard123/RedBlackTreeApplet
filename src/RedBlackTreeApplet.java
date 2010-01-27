6b8976865f657c0317acd57673a883009267a652
Kevin Risden <compuwizard123@gmail.com>
2010-01-27 15:07:15 -0500

    Added RedBlackTreeApplet class

---
+++ src/RedBlackTreeApplet.java
@@ -0,0 +1,16 @@
+import java.applet.Applet;
+import java.awt.*;
+
+
+public class RedBlackTreeApplet extends Applet{
+	public void init() { }
+	 
+	  // This method is mandatory, but can be empty.
+	  public void stop() { }
+	 
+	  // Print a message on the screen (x=20, y=10).
+	  public void paint(Graphics g) {
+	    g.drawString("Hello, world!", 20,10);
+	  }
+
+}
